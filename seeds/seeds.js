d5c3bb2037aac26aba1a3e5e4f54b724e6cbb1b6
Christopher Comella <git.chrisco@gmail.com>
2016-06-28 10:36:20 -0600

    Seeds.js working. Run with $ knex seed:run

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,38 +1,17 @@
-/*
-===========================
-2016-06-27 at 10:00 PM
-===========================
-Work in progress. Haven't tested yet.
-Will be a miracle if it works out of the box.
-*/
-
 exports.seed = function(knex, Promise) {
-	// Deletes ALL existing entries
-	return knex('happy_hour').del()
-		.then(function() {
-			return knex('favorite').del();
-		})
-		.then(function() {
-			return knex('location').del();
-		})
-		.then(function() {
-			return knex('neighborhood').del();
-		})
-		.then(function() {
-			return knex('contributor').del();
-		})
+	return knex.raw("TRUNCATE happy_hour, favorite, location, location, contributor, neighborhood RESTART IDENTITY CASCADE")
 		.then(function() {
 			return Promise.all([
 				// Inserts seed entries
 				knex('contributor').insert({
-					email: 'foo@bar.com', // Validation? Sanitization? Of "ALL" fields??
+					email: 'foo@bar.com',
 					password: 'foobar',
-					isAdmin: false
+					isadmin: true
 				}),
 				knex('contributor').insert({
 					email: 'foo2@bar.com',
 					password: 'foo2bar',
-					isAdmin: true
+					isadmin: false
 				}),
 				knex('neighborhood').insert({
 					name: 'LoHi',
@@ -54,18 +33,18 @@ exports.seed = function(knex, Promise) {
 			return Promise.all([
 					knex('location').insert({
 						name: 'Brider',
-						address: '1644 Platte St, Denver, CO 80202', // What format do we want this? Validation?
-						url: 'http://denverbrider.com', // What format do we want this? Validation?
-						image_url: 'http://ecospace.com/wp-content/uploads/2015/12/IMG_0324.jpg', // Validation?
-						contributor_id: findEmail('foo@bar.com', data[1]),
-						neighborhood_name: findNeighborhood('LoHi', data[1])
+						address: '1644 Platte St, Denver, CO 80202',
+						url: 'http://denverbrider.com',
+						image_url: 'http://ecospace.com/wp-content/uploads/2015/12/IMG_0324.jpg',
+						contributor_id: findEmail('foo@bar.com', data[0]),
+						neighborhood_name: findNeighborhood('LoHi', data[1]) // DOESN"T WORK. WHY?
 					}),
 					knex('location').insert({
 						name: 'Brider2',
 						address: '1644 Platte St, Denver, CO 80202',
 						url: 'http://denverbrider.com',
 						image_url: 'http://ecospace.com/wp-content/uploads/2015/12/IMG_0324.jpg',
-						contributor_id: findEmail('foo2@bar.com', data[1]),
+						contributor_id: findEmail('foo2@bar.com', data[0]),
 						neighborhood_name: findNeighborhood('LoHi2', data[1])
 					})
 				])
@@ -92,20 +71,20 @@ exports.seed = function(knex, Promise) {
 				]),
 				knex('happy_hour').insert({
 					day: 'monday',
-					start: '4:00 PM', // What format do we want times? Validation?
-					end: '7:00 PM',
+					start: '16:00',
+					end: '15:00',
 					location_id: findLocation('Brider', data[1]),
 					contributor_id: findEmail('foo@bar.com', data[0])
 				}),
 				knex('happy_hour').insert({
 					day: 'monday',
-					start: '4:30 PM',
-					end: '6:30 PM',
+					start: '16:30',
+					end: '18:30',
 					location_id: findLocation('Brider2', data[1]),
 					contributor_id: findEmail('foo2@bar.com', data[0])
 				})
 				.then(function() {
-					return data; // Necessary? (If not, remove.)
+					return data;
 				});
 		});
 };
@@ -122,7 +101,7 @@ function findEmail(email, list) {
 function findNeighborhood(name, list) {
 	for (var i = 0; i < list.length; i++) {
 		if (name === list[i].name) {
-			return list[i].id;
+			return list[i].name;
 		}
 	}
 }

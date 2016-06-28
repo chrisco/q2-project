d7b97f5134f256f8af7afad18f54bf49336a868c
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2016-06-28 08:16:52 -0600

    change api

--- db/api.js
+++ db/api.js
@@ -2,9 +2,8 @@ var knex = require('./knex');
 
 module.exports = {
     Contributor: {
-        findContributorByEmail: email => knex('contributor').where('email', email).returning('id').first(),
-        findContributorById: id => knex('contributor').where('id', id).first(),
-        createContributor: body => knex('contributor').insert(body, 'id')
+        getContributorById: id => knex('contributor').where('id', id).first(),
+        addContributor: body => knex('contributor').insert(body, 'id')
     },
     Favorite: {
 
